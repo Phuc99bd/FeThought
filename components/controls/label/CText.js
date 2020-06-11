b5ee755e21b758b46bda3b704ef15caa3e6a20f3
minhask <nguyenvanquocdh1995@gmail.com>
2020-06-11 10:30:29 +0700

    FirstUpdateCreateFolder

---
+++ components/controls/label/CText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HEIGHT = 40;
+const HEIGHT_SMALL = 30;
+const HEIGHT_LARGE = 50;
+
+export default function CText(props){
+    
+}
