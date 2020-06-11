d67f68f374c58a3ca41ef03ee56d27207e0e9a36
Cong Phuc <phuccog@gmail.com>
2020-06-11 17:57:24 +0700

    update structure

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-import {Text , View ,StyleSheet, AppRegistry} from "react-native";
 import Login from "./screens/auth/login"
 
 export default function App() {
@@ -10,32 +8,3 @@ export default function App() {
 
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 25,
-    padding: 10
-  },
-  header: {
-    fontSize: 20
-  },
-  nav: {
-    flexDirection: "row",
-    justifyContent: "space-around"
-  },
-  navItem: {
-    flex: 1,
-    alignItems: "center",
-    padding: 10
-  },
-  subNavItem: {
-    padding: 5
-  },
-  topic: {
-    textAlign: "center",
-    fontSize: 15
-  }
-});
-
-AppRegistry.registerComponent("MyApp",()=>App);
